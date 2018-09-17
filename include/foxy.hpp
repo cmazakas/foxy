2109b26b886b49890573d726d48a5d0b1579d5e5
LeonineKing1199 <christian.mazakas@gmail.com>
2018-09-16 17:22:59 -0700

    Added single include header

---
+++ include/foxy.hpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2018-2018 Christian Mazakas (christian dot mazakas at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/LeonineKing1199/f3
+//
+
+#ifndef FOXY_HPP_
+#define FOXY_HPP_
+
+#include <foxy/client_session.hpp>
+#include <foxy/log.hpp>
+#include <foxy/multi_stream.hpp>
+#include <foxy/proxy.hpp>
+#include <foxy/server_session.hpp>
+#include <foxy/session.hpp>
+#include <foxy/shared_handler_ptr.hpp>
+#include <foxy/utility.hpp>
+
+#endif // FOXY_HPP_
