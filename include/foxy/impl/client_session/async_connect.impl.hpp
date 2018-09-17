d080494f61edd0f6cba0c746117de134422fdc71
LeonineKing1199 <christian.mazakas@gmail.com>
2018-09-16 17:16:11 -0700

    Updated licensing

--- include/foxy/impl/client_session/async_connect.impl.hpp
+++ include/foxy/impl/client_session/async_connect.impl.hpp
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2018-2018 Christian Mazakas (christian dot mazakas at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/LeonineKing1199/f3
+//
+
 #ifndef FOXY_IMPL_CLIENT_SESSION_ASYNC_CONNECT_IMPL_HPP_
 #define FOXY_IMPL_CLIENT_SESSION_ASYNC_CONNECT_IMPL_HPP_
 
--- src/proxy.cpp
+++ src/proxy.cpp
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2018-2018 Christian Mazakas (christian dot mazakas at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/LeonineKing1199/f3
+//
+
 #include "foxy/proxy.hpp"
 #include "foxy/server_session.hpp"
 #include "foxy/client_session.hpp"
