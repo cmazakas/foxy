b60740c067adb7f10da81cd2500bf2e30280d654
LeonineKing1199 <christian.mazakas@gmail.com>
2018-09-17 20:09:10 -0700

    Fixed typo in CML file

--- foxy-travis.cmake
+++ foxy-travis.cmake
@@ -1,6 +1,6 @@
 set(FOXY_TESTING ON)
 set(CMAKE_BUILD_TYPE Debug)
 set(CMAKE_CXX_COMPILER "g++-7")
-set(VCPKG_TARGET_TRIPLET, "x64-linux-cxx14")
+set(VCPKG_TARGET_TRIPLET "x64-linux-cxx14")
 
 include(/home/travis/build/LeonineKing1199/f3/vcpkg/scripts/buildsystems/vcpkg.cmake)
