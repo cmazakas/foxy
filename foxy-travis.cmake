24daef85906b573b6dd8f522f1517df514a1e744
LeonineKing1199 <christian.mazakas@gmail.com>
2018-09-17 18:54:19 -0700

    Fixed cmake include call

--- foxy-travis.cmake
+++ foxy-travis.cmake
@@ -2,4 +2,4 @@ set(FOXY_TESTING ON)
 set(CMAKE_BUILD_TYPE Debug)
 set(CMAKE_CXX_COMPILER "g++-7")
 
-include(${VCPKG_TOOLCHAIN_FILE})
+include(/home/travis/build/LeonineKing1199/f3/vcpkg/scripts/buildsystems/vcpkg.cmake)
