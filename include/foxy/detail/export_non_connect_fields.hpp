b0e1060fff327db280e449d80ae05f62d594ff7a
LeonineKing1199 <LeonineKing1199@users.noreply.github.com>
2018-09-24 18:20:38 -0700

    Back-Merge of Develop (#2)
    * Updated licensing
    
    * Added single include header
    
    * Refactored session class into impl file
    
    * Updated project-local header inclusion syntax
    
    * Added README
    
    * Began work on testing header partitioning
    
    * Wrote initial failing test, fixed README
    
    * Continued work on headers
    
    * Updated licensing
    
    * Added single include header
    
    * Refactored session class into impl file
    
    * Updated project-local header inclusion syntax
    
    * Added README
    
    * Began work on testing header partitioning
    
    * Wrote initial failing test, fixed README
    
    * Continued work on headers
    
    * Updated README
    
    * Got header partitioning working
    
    * Updated safelist

--- include/foxy/detail/export_non_connect_fields.hpp
+++ include/foxy/detail/export_non_connect_fields.hpp
@@ -13,10 +13,17 @@
 #include <boost/beast/http/field.hpp>
 #include <boost/beast/http/fields.hpp>
 #include <boost/beast/http/type_traits.hpp>
+
+#include <boost/beast/http/rfc7230.hpp>
+
 #include <boost/utility/string_view.hpp>
-#include <boost/range/algorithm/for_each.hpp>
+#include <boost/range/end.hpp>
+#include <boost/range/algorithm.hpp>
+
 #include <type_traits>
 #include <vector>
+#include <memory>
+#include <iterator>
 
 namespace foxy
 {
@@ -33,8 +40,10 @@ export_non_connect_fields(Fields& src, Fields& dst);
 } // detail
 } // foxy
 
-// HTTP RFC 7230: https://tools.ietf.org/html/rfc7230#section-3.2.2
+// Using this, we know the user is allowed to specify Connection multiple
+// times in the fields
 //
+// HTTP RFC 7230: https://tools.ietf.org/html/rfc7230#section-3.2.2
 // A sender MUST NOT generate multiple header fields with the same field
 // name in a message unless either the entire field value for that
 // header field is defined as a comma-separated list [i.e., #(values)]
@@ -48,30 +57,58 @@ void
 foxy::detail::
 export_non_connect_fields(Fields& src, Fields& dst)
 {
-  namespace http = boost::beast::http;
-
   // first collect all the Connection options into one coherent list
   //
+  namespace http = boost::beast::http;
+  namespace rng  = boost::range;
+
+  using allocator_type = typename std::allocator_traits<
+    typename Fields::allocator_type
+  >::template rebind_alloc<boost::string_view>;
+
+  auto connect_opts =
+    std::vector<boost::string_view, allocator_type>(src.get_allocator());
+  connect_opts.reserve(128);
+
   auto const connect_fields = src.equal_range(http::field::connection);
 
-  auto connect_token_names =
-    std::vector<
-      boost::string_view,
-      typename Fields::allocator_type
-    >(src.get_allocator());
+  auto out = std::back_inserter(connect_opts);
 
-  connect_token_names.reserve(128);
-
-  boost::range::for_each(
+  rng::for_each(
     connect_fields,
-    [](auto const& connect_field)
+    [out]
+    (auto const& connect_field)
     {
-
+      rng::copy(http::token_list(connect_field.value()), out);
     });
+
+  rng::sort(connect_opts);
+  rng::unique(connect_opts);
 
   // iterate the `src` fields, moving any non-connect headers and the
   // corresponding tokens to the `dst` fields
   //
+  auto const is_connect_opt =
+    [&connect_opts]
+    (typename Fields::value_type const& field) -> bool
+    {
+      if (field.name() == http::field::connection) {
+        return true;
+      }
+      return rng::find(connect_opts, field.name_string()) != boost::end(connect_opts);
+    };
+
+  for (auto it = src.begin(); it != src.end(); ++it) {
+    auto const& field = *it;
+    if (is_connect_opt(field)) {
+      continue;
+    }
+    dst.insert(field.name_string(), field.value());
+    it = src.erase(it);
+    if (it == src.end()) {
+      break;
+    }
+  }
 }
 
 #endif // FOXY_DETAIL_EXPORT_NON_CONNECT_FIELDS_HPP_
