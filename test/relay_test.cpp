ccf799ef493df275b454cf293fcdfcbd7d527c72
LeonineKing1199 <christian.mazakas@gmail.com>
2018-11-03 15:40:54 -0700

    Got clang-format working

--- test/relay_test.cpp
+++ test/relay_test.cpp
@@ -1,5 +1,6 @@
 //
-// Copyright (c) 2018-2018 Christian Mazakas (christian dot mazakas at gmail dot com)
+// Copyright (c) 2018-2018 Christian Mazakas (christian dot mazakas at gmail dot
+// com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -7,15 +8,15 @@
 // Official repository: https://github.com/LeonineKing1199/f3
 //
 
-#include <foxy/detail/relay.hpp>
 #include <foxy/session.hpp>
+#include <foxy/detail/relay.hpp>
 
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/spawn.hpp>
 
-#include <boost/beast/http.hpp>
 #include <boost/beast/core/ostream.hpp>
 #include <boost/beast/experimental/test/stream.hpp>
+#include <boost/beast/http.hpp>
 
 #include <boost/beast/core/ostream.hpp>
 
@@ -56,19 +57,16 @@ TEST_CASE("Our async HTTP relay")
     server.stream.plain().connect(res_stream);
     client.stream.plain().connect(req_stream);
 
-    asio::spawn(
-      [&](asio::yield_context yield) mutable
-      {
-        foxy::detail::async_relay(server, client, yield);
-      });
+    asio::spawn([&](asio::yield_context yield) mutable {
+      foxy::detail::async_relay(server, client, yield);
+    });
 
     io.run();
 
     CHECK(req_stream.str() == "GET / HTTP/1.1\r\n\r\n");
-    CHECK(
-      res_stream.str() == "HTTP/1.1 200 OK\r\n"
-                          "Content-Length: 58\r\n\r\n"
-                          "I bestow the heads of virgins and the first-born sons!!!!\n"
-    );
+    CHECK(res_stream.str() ==
+          "HTTP/1.1 200 OK\r\n"
+          "Content-Length: 58\r\n\r\n"
+          "I bestow the heads of virgins and the first-born sons!!!!\n");
   }
 }
