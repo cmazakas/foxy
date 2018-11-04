95d3d4d9f7ea212b4c72363f3bde4aab2d9a2475
LeonineKing1199 <christian.mazakas@gmail.com>
2018-11-04 07:14:06 -0800

    Fixed repo link in licensing

--- include/foxy/detail/relay.hpp
+++ include/foxy/detail/relay.hpp
@@ -1,10 +1,11 @@
 //
-// Copyright (c) 2018-2018 Christian Mazakas (christian dot mazakas at gmail dot com)
+// Copyright (c) 2018-2018 Christian Mazakas (christian dot mazakas at gmail dot
+// com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// Official repository: https://github.com/LeonineKing1199/f3
+// Official repository: https://github.com/LeonineKing1199/foxy
 //
 
 #ifndef FOXY_DETAIL_RELAY_HPP_
@@ -26,12 +27,10 @@ namespace foxy
 {
 namespace detail
 {
-
 template <class Stream, class RelayHandler>
 struct relay_op : boost::asio::coroutine
 {
 private:
-
   struct state
   {
     std::array<char, 2048> buffer;
@@ -40,30 +39,29 @@ private:
     ::foxy::basic_session<Stream>& client;
 
     boost::optional<
-      boost::beast::http::request_parser<boost::beast::http::buffer_body>
-    > req_parser;
+      boost::beast::http::request_parser<boost::beast::http::buffer_body>>
+      req_parser;
 
     boost::optional<
-      boost::beast::http::request_serializer<boost::beast::http::buffer_body>
-    > req_sr;
+      boost::beast::http::request_serializer<boost::beast::http::buffer_body>>
+      req_sr;
 
     boost::optional<
-      boost::beast::http::response_parser<boost::beast::http::buffer_body>
-    > res_parser;
+      boost::beast::http::response_parser<boost::beast::http::buffer_body>>
+      res_parser;
 
     boost::optional<
-      boost::beast::http::response_serializer<boost::beast::http::buffer_body>
-    > res_sr;
+      boost::beast::http::response_serializer<boost::beast::http::buffer_body>>
+      res_sr;
 
     boost::asio::executor_work_guard<decltype(server.get_executor())> work;
 
-    explicit state(
-      RelayHandler const&            handler,
-      ::foxy::basic_session<Stream>& server_,
-      ::foxy::basic_session<Stream>& client_)
-    : server(server_)
-    , client(client_)
-    , work(server.get_executor())
+    explicit state(RelayHandler const&            handler,
+                   ::foxy::basic_session<Stream>& server_,
+                   ::foxy::basic_session<Stream>& client_)
+      : server(server_)
+      , client(client_)
+      , work(server.get_executor())
     {
     }
   };
@@ -76,24 +74,23 @@ public:
   relay_op(relay_op&&)      = default;
 
   template <class DeducedHandler>
-  relay_op(
-    ::foxy::basic_session<Stream>& server,
-    ::foxy::basic_session<Stream>& client,
-    DeducedHandler&&               handler)
-  : p_(std::forward<DeducedHandler>(handler), server, client)
+  relay_op(::foxy::basic_session<Stream>& server,
+           ::foxy::basic_session<Stream>& client,
+           DeducedHandler&&               handler)
+    : p_(std::forward<DeducedHandler>(handler), server, client)
   {
   }
 
   using executor_type = boost::asio::associated_executor_t<
     RelayHandler,
-    decltype((std::declval<::foxy::basic_session<Stream>&>().get_executor()))
-  >;
+    decltype((std::declval<::foxy::basic_session<Stream>&>().get_executor()))>;
 
   using allocator_type = boost::asio::associated_allocator_t<RelayHandler>;
 
   auto get_executor() const noexcept -> executor_type
   {
-    return boost::asio::get_associated_executor(p_.handler(), p_->server.get_executor());
+    return boost::asio::get_associated_executor(p_.handler(),
+                                                p_->server.get_executor());
   }
 
   auto get_allocator() const noexcept -> allocator_type
@@ -101,20 +98,16 @@ public:
     return boost::asio::get_associated_allocator(p_.handler());
   }
 
-  auto
-  operator()(
-    boost::system::error_code ec,
-    std::size_t const         bytes_transferred,
-    bool const                is_continuation = true) -> void;
+  auto operator()(boost::system::error_code ec,
+                  std::size_t const         bytes_transferred,
+                  bool const                is_continuation = true) -> void;
 };
 
 template <class Stream, class RelayHandler>
-auto
-relay_op<Stream, RelayHandler>::
-operator()(
-  boost::system::error_code ec,
-  std::size_t const         bytes_transferred,
-  bool const                is_continuation) -> void
+auto relay_op<Stream, RelayHandler>::
+     operator()(boost::system::error_code ec,
+           std::size_t const         bytes_transferred,
+           bool const                is_continuation) -> void
 {
   using namespace std::placeholders;
   using boost::beast::bind_handler;
@@ -153,7 +146,8 @@ operator()(
         if (ec == http::error::need_buffer) { ec = {}; }
         if (ec) { goto upcall; }
 
-        s.req_parser->get().body().size = s.buffer.size() - s.req_parser->get().body().size;
+        s.req_parser->get().body().size =
+          s.buffer.size() - s.req_parser->get().body().size;
         s.req_parser->get().body().data = s.buffer.data();
         s.req_parser->get().body().more = !s.req_parser->is_done();
 
@@ -166,8 +160,7 @@ operator()(
       s.client.async_write(*s.req_sr, std::move(*this));
       if (ec == http::error::need_buffer) { ec = {}; }
       if (ec) { goto upcall; }
-    }
-    while (!s.req_parser->is_done() && !s.req_sr->is_done());
+    } while (!s.req_parser->is_done() && !s.req_sr->is_done());
 
     BOOST_ASIO_CORO_YIELD
     s.client.async_read_header(*s.res_parser, std::move(*this));
@@ -187,7 +180,8 @@ operator()(
         if (ec == http::error::need_buffer) { ec = {}; }
         if (ec) { goto upcall; }
 
-        s.res_parser->get().body().size = s.buffer.size() - s.res_parser->get().body().size;
+        s.res_parser->get().body().size =
+          s.buffer.size() - s.res_parser->get().body().size;
         s.res_parser->get().body().data = s.buffer.data();
         s.res_parser->get().body().more = !s.res_parser->is_done();
 
@@ -200,8 +194,7 @@ operator()(
       s.server.async_write(*s.res_sr, std::move(*this));
       if (ec == http::error::need_buffer) { ec = {}; }
       if (ec) { goto upcall; }
-    }
-    while (!s.res_parser->is_done() && !s.res_sr->is_done());
+    } while (!s.res_parser->is_done() && !s.res_sr->is_done());
 
     {
       auto work = std::move(s.work);
@@ -219,29 +212,25 @@ operator()(
 }
 
 template <class Stream, class RelayHandler>
-auto
-async_relay(
-  ::foxy::basic_session<Stream>& server,
-  ::foxy::basic_session<Stream>& client,
-  RelayHandler&&                 handler
-) -> BOOST_ASIO_INITFN_RESULT_TYPE(RelayHandler, void(boost::system::error_code, std::size_t))
+auto async_relay(::foxy::basic_session<Stream>& server,
+                 ::foxy::basic_session<Stream>& client,
+                 RelayHandler&&                 handler)
+  -> BOOST_ASIO_INITFN_RESULT_TYPE(RelayHandler,
+                                   void(boost::system::error_code, std::size_t))
 {
-  boost::asio::async_completion<
-    RelayHandler, void(boost::system::error_code, std::size_t)
-  >
-  init(handler);
+  boost::asio::async_completion<RelayHandler,
+                                void(boost::system::error_code, std::size_t)>
+    init(handler);
 
-  relay_op<
-    Stream,
-    BOOST_ASIO_HANDLER_TYPE(
-      RelayHandler,
-      void(boost::system::error_code, std::size_t))
-  >(server, client, std::move(init.completion_handler))({}, 0, false);
+  relay_op<Stream,
+           BOOST_ASIO_HANDLER_TYPE(
+             RelayHandler, void(boost::system::error_code, std::size_t))>(
+    server, client, std::move(init.completion_handler))({}, 0, false);
 
   return init.result.get();
 }
 
-} // detail
-} // foxy
+} // namespace detail
+} // namespace foxy
 
 #endif // FOXY_DETAIL_RELAY_HPP_
