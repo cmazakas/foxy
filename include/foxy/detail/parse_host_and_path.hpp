5e0e07e4f6611b963b4ad615373fe967b80dcb1b
LeonineKing1199 <christian.mazakas@gmail.com>
2018-11-25 10:24:13 -0800

    Got prototype of absolute uri parsing working

--- include/foxy/detail/parse_host_and_path.hpp
+++ include/foxy/detail/parse_host_and_path.hpp
@@ -11,11 +11,41 @@
 #ifndef FOXY_DETAIL_PARSE_HOST_AND_PATH_HPP_
 #define FOXY_DETAIL_PARSE_HOST_AND_PATH_HPP_
 
+#include <boost/spirit/home/x3.hpp>
+
+#include <boost/fusion/adapted/std_tuple.hpp>
+#include <boost/fusion/include/std_tuple.hpp>
+
+#include <boost/utility/string_view.hpp>
+
+#include <tuple>
+
 namespace foxy
 {
 namespace detail
 {
+template <class String>
+auto
+parse_host_and_path(boost::string_view const              uri,
+                    std::tuple<String&, String&, String&> attribute) -> void;
 } // namespace detail
 } // namespace foxy
+
+template <class String>
+auto
+foxy::detail::parse_host_and_path(
+  boost::string_view const              uri,
+  std::tuple<String&, String&, String&> attribute) -> void
+{
+  namespace x3 = boost::spirit::x3;
+
+  auto       begin = uri.begin();
+  auto const end   = uri.end();
+
+  x3::parse(begin, end,
+            x3::lit("http") >> -x3::lit("s") >> "://" >> +(x3::char_ - ":") >>
+              -(":" >> +(x3::char_ - "/")) >> +x3::char_,
+            attribute);
+}
 
 #endif // FOXY_DETAIL_PARSE_HOST_AND_PATH_HPP_
--- test/parse_host_and_path_test.cpp
+++ test/parse_host_and_path_test.cpp
@@ -1,7 +1,10 @@
 #include <foxy/detail/parse_host_and_path.hpp>
+
 #include <boost/utility/string_view.hpp>
+
 #include <tuple>
 #include <string>
+#include <functional>
 
 #include <catch2/catch.hpp>
 
@@ -14,6 +17,15 @@ TEST_CASE("Our absolute URI parser...")
     auto host   = std::string();
     auto port   = std::string();
     auto target = std::string();
+
+    host.reserve(32);
+    port.reserve(32);
+    target.reserve(32);
+
+    auto attribute =
+      std::make_tuple(std::ref(host), std::ref(port), std::ref(target));
+
+    foxy::detail::parse_host_and_path(uri, attribute);
 
     CHECK(host == "www.google.com");
     CHECK(port == "1337");
