b381bcaf0e7cef1d8193514e7926951e8ce8a3bf
LeonineKing1199 <christian.mazakas@gmail.com>
2018-11-27 19:39:01 -0800

    Began work on uri parsing

--- include/foxy/detail/parse_host_and_path.hpp
+++ include/foxy/detail/parse_host_and_path.hpp
@@ -22,6 +22,17 @@
 
 namespace foxy
 {
+namespace uri
+{
+static auto const sub_delims =
+  boost::spirit::x3::lit("!") | boost::spirit::x3::lit("$") |
+  boost::spirit::x3::lit("&") | boost::spirit::x3::lit("'") |
+  boost::spirit::x3::lit("(") | boost::spirit::x3::lit(")") |
+  boost::spirit::x3::lit("*") | boost::spirit::x3::lit("+") |
+  boost::spirit::x3::lit(",") | boost::spirit::x3::lit(";") |
+  boost::spirit::x3::lit("=");
+}
+
 namespace detail
 {
 template <class String>
@@ -38,6 +49,50 @@ foxy::detail::parse_host_and_path(
   std::tuple<String&, String&, String&> attribute) -> void
 {
   namespace x3 = boost::spirit::x3;
+
+  // auto chars = x3::char_set<boost::spirit::char_encoding::ascii>("123");
+
+  using char_set = x3::char_set<boost::spirit::char_encoding::ascii>;
+
+  auto const sub_delims = x3::lit("!") | x3::lit("$") | x3::lit("&") |
+                          x3::lit("'") | x3::lit("(") | x3::lit(")") |
+                          x3::lit("*") | x3::lit("+") | x3::lit(",") |
+                          x3::lit(";") | x3::lit("=");
+
+  auto const gen_delims = x3::lit(":") | x3::lit("|") | x3::lit("?") |
+                          x3::lit("#") | x3::lit("[") | x3::lit("]") |
+                          x3::lit("@");
+
+  auto const reserved   = gen_delims | sub_delims;
+  auto const unreserved = x3::alpha | x3::digit | "-" | "." | "_" | "~";
+
+  auto const pct_encoded = x3::lit("%") >> x3::hex >> x3::hex;
+
+  auto const pchar = unreserved | pct_encoded | sub_delims | ":" | "@";
+
+  auto const query    = *(pchar | "/" | "?");
+  auto const fragment = *(pchar | "/" | "?");
+
+  auto const segment       = *pchar;
+  auto const segment_nz    = pchar;
+  auto const segment_nz_nc = unreserved | pct_encoded | sub_delims | "@";
+
+  auto const path_abempty  = *("/" >> segment);
+  auto const path_absolute = "/" >> -(segment_nz >> *("/" >> segment));
+  auto const path_noscheme = segment_nz_nc >> *("/" >> segment);
+  auto const path_rootless = segment_nz >> *("/" >> segment);
+  auto const path_empty    = x3::eps;
+
+  auto const path =
+    path_abempty | path_absolute | path_noscheme | path_rootless | path_empty;
+
+  auto const reg_name = *(unreserved | pct_encoded | sub_delims);
+
+  auto const dec_octet = x3::digit                               // 0-9
+                         | (char_set("1-9") >> x3::digit)        // 10-99
+                         | ("1" >> x3::digit >> x3::digit)       // 100-199
+                         | ("2" >> char_set("0-4") >> x3::digit) // 200-249
+                         | ("25" >> char_set("0-5"));            // 250-255
 
   auto       begin = uri.begin();
   auto const end   = uri.end();
--- test/parse_host_and_path_test.cpp
+++ test/parse_host_and_path_test.cpp
@@ -5,11 +5,28 @@
 #include <tuple>
 #include <string>
 #include <functional>
+#include <vector>
+#include <algorithm>
 
 #include <catch2/catch.hpp>
 
+namespace x3 = boost::spirit::x3;
+
 TEST_CASE("Our absolute URI parser...")
 {
+  SECTION("should match the sub delims")
+  {
+    auto const delims = std::vector<boost::string_view>{
+      "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="};
+
+    auto const matched_all_sub_delims =
+      std::all_of(delims.begin(), delims.end(), [](auto const delim) -> bool {
+        return x3::parse(delim.begin(), delim.end(), foxy::uri::sub_delims);
+      });
+
+    CHECK(matched_all_sub_delims);
+  }
+
   SECTION("should extract the host, port and target")
   {
     auto const uri = boost::string_view("http://www.google.com:1337/");
