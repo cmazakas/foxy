2fd190eca0ed6e717239a60a69e4551eb303d419
LeonineKing1199 <christian.mazakas@gmail.com>
2019-02-14 08:20:30 -0800

    Updated OSX toolchain, updated Docker stuff

--- foxy-osx.cmake
+++ foxy-osx.cmake
@@ -1,4 +1,4 @@
 set(FOXY_TESTING ON)
 set(Boost_USE_STATIC_LIBS ON)
-set(BOOST_ROOT "/Users/exbigboss/boosts/68/install")
+set(BOOST_ROOT "/Users/exbigboss/boosts/69/install")
 include("/Users/exbigboss/vcpkg/scripts/buildsystems/vcpkg.cmake")
