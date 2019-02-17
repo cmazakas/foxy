931d41bc326ff3369e753989e3473dd9d4fee630
LeonineKing1199 <christian.mazakas@gmail.com>
2019-02-17 10:54:52 -0800

    Updated toolchain files in lieu of core lib

--- foxy-osx.cmake
+++ foxy-osx.cmake
@@ -1,4 +1,5 @@
 set(FOXY_TESTING ON)
 set(Boost_USE_STATIC_LIBS ON)
 set(BOOST_ROOT "/Users/exbigboss/boosts/69/install")
+add_compile_definitions("BOOST_ASIO_NO_DEPRECATED")
 include("/Users/exbigboss/vcpkg/scripts/buildsystems/vcpkg.cmake")
