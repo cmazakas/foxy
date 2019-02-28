7b5866ee77960e7a2ed6f446e988344d6136569d
LeonineKing1199 <christian.mazakas@gmail.com>
2019-02-27 19:06:59 -0800

    No longer log SSL short reads

--- src/log.cpp
+++ src/log.cpp
@@ -1,18 +1,23 @@
 //
-// Copyright (c) 2018-2019 Christian Mazakas (christian dot mazakas at gmail dot
-// com)
+// Copyright (c) 2018-2019 Christian Mazakas (christian dot mazakas at gmail dot com)
 //
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 // Official repository: https://github.com/LeonineKing1199/foxy
 //
 
 #include <foxy/log.hpp>
+#include <boost/asio/ssl/error.hpp>
+
+namespace net = boost::asio;
 
 auto
-foxy::log_error(boost::system::error_code const ec,
-                boost::string_view const        what) -> void
+foxy::log_error(boost::system::error_code const ec, boost::string_view const what) -> void
 {
+  // don't bother printing ssl short reads
+  // if an SSL stream truly _is_ truncated, Beast's parser errors out with a partial_message
+  //
+  if (ec == net::ssl::error::stream_truncated) { return; }
   std::cerr << what << " : " << ec.message() << "\n";
 }
