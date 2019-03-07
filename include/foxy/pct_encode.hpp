c38837f1a1cd2ab1f69da188c7437462e5a18564
LeonineKing1199 <christian.mazakas@gmail.com>
2019-03-06 19:37:45 -0800

    Added code comments

--- include/foxy/pct_encode.hpp
+++ include/foxy/pct_encode.hpp
@@ -17,6 +17,20 @@ namespace foxy
 {
 namespace uri
 {
+//
+// to_utf8_encoding takes the Boost.Locale type `utf::code_point` which is capable of holding any
+// Unicode code point. We convert the number to an unsigned 32 bit integer which represents the 1-4
+// byte UTF-8 binary encoding scheme seen here:
+// https://en.wikipedia.org/wiki/UTF-8
+//
+// UTF-8 encoding writes the representation of the code point's value to the binary templates
+// described below:
+//
+// 7 bits =>  [U+0000,  U+007F]   => 0xxxxxxx
+// 11 bits => [U+0080,  U+07FF]   => 110xxxxx 10xxxxxx
+// 16 bits => [U+0800,  U+FFFF]   => 1110xxxx 10xxxxxx 10xxxxxx
+// 21 bits => [U+10000, U+10FFFF] => 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+//
 auto
 to_utf8_encoding(boost::locale::utf::code_point const code_point) -> std::uint32_t;
 
