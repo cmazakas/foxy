204ed4062bf345103b33e293f641b93949696bf2
LeonineKing1199 <christian.mazakas@gmail.com>
2019-03-31 14:52:45 -0700

    Added insecure warning suppression

--- foxy.cmake
+++ foxy.cmake
@@ -4,5 +4,5 @@ set(Boost_DIR "/Users/cmaza/cpp/boost-170/lib/cmake/Boost-1.70.0")
 
 add_compile_options("/permissive-")
 add_link_options("/NODEFAULTLIB:library" "/debug:fastlink")
-add_compile_definitions("BOOST_ASIO_NO_DEPRECATED")
+add_compile_definitions("BOOST_ASIO_NO_DEPRECATED" "_CRT_SECURE_NO_WARNINGS")
 include("/Users/cmaza/cpp/vcpkg/scripts/buildsystems/vcpkg.cmake")
