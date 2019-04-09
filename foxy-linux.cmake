bfb416ccbf7a6567de3dab3ce1a579653e1c5d87
LeonineKing1199 <christian.mazakas@gmail.com>
2019-04-08 18:43:07 -0700

    Updated toolchain files

--- foxy-linux.cmake
+++ foxy-linux.cmake
@@ -1,10 +1,12 @@
+set(FOXY_BUILD_EXAMPLES ON)
+
 set(Boost_USE_STATIC_LIBS ON)
 set(Boost_DIR "/home/chris/boosts/install/170/lib/cmake/Boost-1.70.0")
-set(BUILD_SHARED_LIBS OFF)
+
+set(BUILD_TESTING ON)
 set(CMAKE_CXX_COMPILER "g++-7")
 set(CMAKE_BUILD_TYPE Debug)
-set(FOXY_BUILD_EXAMPLES ON)
+set(BUILD_SHARED_LIBS OFF)
 
 add_compile_definitions("BOOST_ASIO_NO_DEPRECATED")
 include("/home/chris/vcpkg/scripts/buildsystems/vcpkg.cmake")
-
--- foxy-osx.cmake
+++ foxy-osx.cmake
@@ -1,7 +1,4 @@
-set(FOXY_TESTING ON)
-# set(Boost_USE_STATIC_LIBS ON)
-# set(BOOST_ROOT "/Users/exbigboss/boosts/69/install")
-
+set(BUILD_TESTING ON)
 set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/Users/exbigboss/boosts/70/lib/cmake/Boost-1.70.0")
 
 add_compile_definitions("BOOST_ASIO_NO_DEPRECATED")
