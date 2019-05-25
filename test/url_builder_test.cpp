21c2a8ab9bee9d837cd700798ed6579261675bed
LeonineKing1199 <christian.mazakas@gmail.com>
2019-05-25 11:17:28 -0700

    Added unreserved pct-encoding case

--- test/url_builder_test.cpp
+++ test/url_builder_test.cpp
@@ -76,4 +76,17 @@ TEST_CASE("Our URL builder class...")
 
     CHECK(encoded_host == "%22%23%2f%3c%3e%3f%40%5b%5c%5d%5e%60%7b%7c%7d");
   }
+
+  SECTION("should not pct-encode the ALPHA | DIGIT | -._~")
+  {
+    auto const host =
+      boost::u32string_view(U"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~");
+
+    auto out = std::string(256, '\0');
+
+    auto const encoded_host =
+      boost::string_view(out.data(), foxy::detail::encode_host(host, out.begin()) - out.begin());
+
+    CHECK(encoded_host == "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~");
+  }
 }
