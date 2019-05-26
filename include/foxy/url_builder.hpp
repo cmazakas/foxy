2b7b15e16575a41f4af98e3c91a7824ccdadcd32
LeonineKing1199 <christian.mazakas@gmail.com>
2019-05-26 11:25:14 -0700

    Added query testing

--- include/foxy/url_builder.hpp
+++ include/foxy/url_builder.hpp
@@ -94,6 +94,48 @@ encode_path(boost::u32string_view const host, OutputIterator out) -> OutputItera
 
   return out;
 }
+
+template <class OutputIterator>
+auto
+encode_query(boost::u32string_view const host, OutputIterator out) -> OutputIterator
+{
+  namespace x3    = boost::spirit::x3;
+  namespace utf   = boost::locale::utf;
+  namespace karma = boost::spirit::karma;
+
+  auto pos = host.begin();
+
+  for (auto const code_point : host) {
+    pos = host.begin();
+
+    // no need to encode the normal ascii set
+    //
+    if ((code_point > 32) && (code_point < 127) &&
+        boost::u32string_view(U"\"#:<>@[\\]^`{|}").find(code_point) ==
+          boost::u32string_view::npos) {
+      out = utf::utf_traits<char>::encode(code_point, out);
+      continue;
+    }
+
+    auto buffer = std::array<std::uint8_t, 4>{0xff, 0xff, 0xff, 0xff};
+
+    auto const end = ::foxy::uri::utf8_encode(code_point, buffer.begin());
+
+    for (auto pos = buffer.begin(); pos < end; ++pos) {
+      karma::generate(out, karma::lit("%") << karma::right_align(2, karma::lit("0"))[karma::hex],
+                      *pos);
+    }
+  }
+
+  return out;
+}
+
+template <class OutputIterator>
+auto
+encode_fragment(boost::u32string_view const host, OutputIterator out) -> OutputIterator
+{
+  return encode_query(host, out);
+}
 } // namespace uri
 } // namespace foxy
 
--- test/url_builder_test.cpp
+++ test/url_builder_test.cpp
@@ -77,17 +77,18 @@ TEST_CASE("Our URL builder class...")
     CHECK(encoded_host == "%22%23%2f%3c%3e%3f%40%5b%5c%5d%5e%60%7b%7c%7d");
   }
 
-  SECTION("[host] should not pct-encode the ALPHA | DIGIT | -._~")
+  SECTION("[host] should not pct-encode the ALPHA | DIGIT | -._~ | sub-delims")
   {
-    auto const host =
-      boost::u32string_view(U"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~");
+    auto const host = boost::u32string_view(
+      U"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~!$&'()*+,;=");
 
     auto out = std::string(256, '\0');
 
     auto const encoded_host =
       boost::string_view(out.data(), foxy::uri::encode_host(host, out.begin()) - out.begin());
 
-    CHECK(encoded_host == "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~");
+    CHECK(encoded_host ==
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~!$&'()*+,;=");
   }
 
   SECTION("[path] should not pct-encode normal paths")
@@ -136,5 +137,40 @@ TEST_CASE("Our URL builder class...")
       boost::string_view(out.data(), foxy::uri::encode_path(path, out.begin()) - out.begin());
 
     CHECK(encoded_path == "/%20%21%22%23");
+  }
+
+  SECTION("[query] shouldn't double-encode pct-encoded params in the query")
+  {
+    auto const query = boost::u32string_view(U"?%20%21%22%23");
+    auto       out   = std::string(256, '\0');
+
+    auto const encoded_query =
+      boost::string_view(out.data(), foxy::uri::encode_query(query, out.begin()) - out.begin());
+
+    CHECK(encoded_query == "?%20%21%22%23");
+  }
+
+  SECTION("[query] shouldn't pct-encode the unreserved + sub-delims + /?")
+  {
+    auto const query = boost::u32string_view(
+      U"?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~!$&'()*+,;=/?");
+    auto out = std::string(256, '\0');
+
+    auto const encoded_query =
+      boost::string_view(out.data(), foxy::uri::encode_query(query, out.begin()) - out.begin());
+
+    CHECK(encoded_query ==
+          "?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~!$&'()*+,;=/?");
+  }
+
+  SECTION("[query] should pct-encode seemingly random entries into the ASCII set")
+  {
+    auto const query = boost::u32string_view(U"?//\":#/<>?@[\\]^`{|}");
+    auto       out   = std::string(256, '\0');
+
+    auto const encoded_query =
+      boost::string_view(out.data(), foxy::uri::encode_query(query, out.begin()) - out.begin());
+
+    CHECK(encoded_query == "?//%22%3a%23/%3c%3e?%40%5b%5c%5d%5e%60%7b%7c%7d");
   }
 }
