4524a413e4e299c563321824ddfb60d0da9fa1a0
LeonineKing1199 <christian.mazakas@gmail.com>
2019-05-30 17:02:21 -0700

    Restructured pct encoding files

--- include/foxy/pct_encode.hpp
+++ include/foxy/pct_encode.hpp
@@ -10,15 +10,17 @@
 #ifndef FOXY_PCT_ENCODE_HPP_
 #define FOXY_PCT_ENCODE_HPP_
 
+#include <foxy/uri.hpp>
 #include <foxy/iterator.hpp>
+
+#include <boost/utility/string_view.hpp>
+#include <boost/locale/utf.hpp>
 
 #include <boost/spirit/include/karma_generate.hpp>
 #include <boost/spirit/include/karma_string.hpp>
 #include <boost/spirit/include/karma_sequence.hpp>
 #include <boost/spirit/include/karma_numeric.hpp>
-
-#include <boost/utility/string_view.hpp>
-#include <boost/locale/utf.hpp>
+#include <boost/spirit/include/karma_right_alignment.hpp>
 
 #include <cstdint>
 #include <cstring>
@@ -62,34 +64,123 @@ utf8_encoding(InputIterator begin, InputIterator end, OutputIterator sink) -> Ou
   return sink;
 }
 
-template <class Char, class OutputIterator, class Traits = std::char_traits<Char>>
+template <class OutputIterator>
 auto
-pct_encode(boost::basic_string_view<Char, Traits> const input, OutputIterator sink)
-  -> OutputIterator
+encode_host(boost::u32string_view const host, OutputIterator out) -> OutputIterator
 {
-  namespace karma = boost::spirit::karma;
+  namespace x3    = boost::spirit::x3;
   namespace utf   = boost::locale::utf;
+  namespace karma = boost::spirit::karma;
 
-  auto const points = code_point_view<Char>(input);
+  auto pos = host.begin();
 
-  for (auto const code_point : points) {
-    if (code_point == utf::illegal || code_point == utf::incomplete) { return sink; }
+  auto match = x3::parse(pos, host.end(), unicode::ip_literal | unicode::ip_v4_address);
+  if (match) {
+    for (auto const code_point : host) { out = utf::utf_traits<char>::encode(code_point, out); }
+    return out;
+  }
+
+  for (auto const code_point : host) {
+    pos = host.begin();
 
     // no need to encode the normal ascii set
     //
-    if ((code_point > 32) && (code_point < 123)) { continue; }
+    if ((code_point > 32) && (code_point < 127) &&
+        boost::u32string_view(U"\"#/<>?@[\\]^`{|}").find(code_point) ==
+          boost::u32string_view::npos) {
+      out = utf::utf_traits<char>::encode(code_point, out);
+      continue;
+    }
 
     auto buffer = std::array<std::uint8_t, 4>{0xff, 0xff, 0xff, 0xff};
 
-    auto const end = utf8_encode(code_point, buffer.begin());
+    auto const end = ::foxy::uri::utf8_encode(code_point, buffer.begin());
 
     for (auto pos = buffer.begin(); pos < end; ++pos) {
-      karma::generate(sink, karma::lit("%") << karma::hex, *pos);
+      karma::generate(out, karma::lit("%") << karma::right_align(2, karma::lit("0"))[karma::hex],
+                      *pos);
     }
   }
 
-  return sink;
+  return out;
 }
+
+template <class OutputIterator>
+auto
+encode_path(boost::u32string_view const host, OutputIterator out) -> OutputIterator
+{
+  namespace x3    = boost::spirit::x3;
+  namespace utf   = boost::locale::utf;
+  namespace karma = boost::spirit::karma;
+
+  auto pos = host.begin();
+
+  for (auto const code_point : host) {
+    pos = host.begin();
+
+    // no need to encode the normal ascii set
+    //
+    if ((code_point > 32) && (code_point < 127) &&
+        boost::u32string_view(U"\"#<>?[\\]^`{|}").find(code_point) == boost::u32string_view::npos) {
+      out = utf::utf_traits<char>::encode(code_point, out);
+      continue;
+    }
+
+    auto buffer = std::array<std::uint8_t, 4>{0xff, 0xff, 0xff, 0xff};
+
+    auto const end = ::foxy::uri::utf8_encode(code_point, buffer.begin());
+
+    for (auto pos = buffer.begin(); pos < end; ++pos) {
+      karma::generate(out, karma::lit("%") << karma::right_align(2, karma::lit("0"))[karma::hex],
+                      *pos);
+    }
+  }
+
+  return out;
+}
+
+template <class OutputIterator>
+auto
+encode_query(boost::u32string_view const host, OutputIterator out) -> OutputIterator
+{
+  namespace x3    = boost::spirit::x3;
+  namespace utf   = boost::locale::utf;
+  namespace karma = boost::spirit::karma;
+
+  auto pos = host.begin();
+
+  for (auto const code_point : host) {
+    pos = host.begin();
+
+    // no need to encode the normal ascii set
+    //
+    if ((code_point > 32) && (code_point < 127) &&
+        boost::u32string_view(U"\"#:<>@[\\]^`{|}").find(code_point) ==
+          boost::u32string_view::npos) {
+      out = utf::utf_traits<char>::encode(code_point, out);
+      continue;
+    }
+
+    auto buffer = std::array<std::uint8_t, 4>{0xff, 0xff, 0xff, 0xff};
+
+    auto const end = ::foxy::uri::utf8_encode(code_point, buffer.begin());
+
+    for (auto pos = buffer.begin(); pos < end; ++pos) {
+      karma::generate(out, karma::lit("%") << karma::right_align(2, karma::lit("0"))[karma::hex],
+                      *pos);
+    }
+  }
+
+  return out;
+}
+
+template <class OutputIterator>
+auto
+encode_fragment(boost::u32string_view const host, OutputIterator out) -> OutputIterator
+{
+  return encode_query(host, out);
+}
+
 } // namespace uri
 } // namespace foxy
 
