6b4396eee1948d38c9adf4aaaf1166ae01206d6d
LeonineKing1199 <christian.mazakas@gmail.com>
2019-06-05 17:55:38 -0700

    Updated linux toolchains

---
+++ foxy-linux-clang.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_CXX_COMPILER /usr/bin/clang++-8)
+
+set(BUILD_TESTING ON)
+set(FOXY_BUILD_EXAMPLES ON)
+
+add_compile_definitions("BOOST_ASIO_NO_DEPRECATED")
+
+add_compile_options("-fsanitize=address,undefined")
+add_link_options("-fsanitize=address,undefined")
+
+# add_compile_options("-fsanitize=thread")
+# add_link_options("-fsanitize=thread")
+
+set(Boost_USE_STATIC_LIBS ON)
+set(Boost_DIR "/home/chris/boost-spirit-develop/lib/cmake/Boost-1.71.0")
+
+set(CMAKE_BUILD_TYPE Debug)
+set(BUILD_SHARED_LIBS OFF)
+
+add_compile_definitions("BOOST_ASIO_NO_DEPRECATED")
+
+include("/home/chris/vcpkg/scripts/buildsystems/vcpkg.cmake")
