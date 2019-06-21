e48cec8569891a142f184f21cb3f16ec0c5f65af
LeonineKing1199 <christian.mazakas@gmail.com>
2019-06-20 18:44:29 -0700

    Condensed test fluff

--- test/pct_decode_test.cpp
+++ test/pct_decode_test.cpp
@@ -14,16 +14,11 @@ TEST_CASE("Our pct-decoding function...")
   {
     auto const input = boost::string_view("www.%c5%bc%c3%b3%c5%82%c4%87.pl");
 
-    auto bytes       = std::array<char, 256>{0};
-    auto code_points = std::array<char32_t, 256>{0};
+    auto bytes = std::array<char, 256>{0};
 
-    auto const point_view = foxy::code_point_view<char>(
-      boost::string_view(bytes.data(), foxy::pct_decode(input, bytes.begin()) - bytes.begin()));
+    auto const decoded_view =
+      boost::string_view(bytes.data(), foxy::pct_decode(input, bytes.begin()) - bytes.begin());
 
-    auto const decoded_view = boost::u32string_view(
-      code_points.data(),
-      std::copy(point_view.begin(), point_view.end(), code_points.begin()) - code_points.begin());
-
-    CHECK(decoded_view == U"www.\u017C\u00F3\u0142\u0107.pl");
+    CHECK(decoded_view == u8"www.\u017C\u00F3\u0142\u0107.pl");
   }
 }
