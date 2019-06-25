e5e452f898de9efa32c456672115070d0c620dbb
LeonineKing1199 <christian.mazakas@gmail.com>
2019-06-24 17:12:21 -0700

    Added except/noexcept versions, updated tests

--- test/pct_decode_test.cpp
+++ test/pct_decode_test.cpp
@@ -17,11 +17,13 @@ TEST_CASE("Our pct-decoding function...")
     auto const input = boost::string_view("www.%c5%bc%c3%b3%c5%82%c4%87.pl");
 
     auto bytes = std::array<char, 256>{0};
+    auto ec    = boost::system::error_code();
 
-    auto const decoded_view =
-      boost::string_view(bytes.data(), foxy::uri::pct_decode(input, bytes.begin()) - bytes.begin());
+    auto const decoded_view = boost::string_view(
+      bytes.data(), foxy::uri::pct_decode(input, bytes.begin(), ec) - bytes.begin());
 
     CHECK(decoded_view == u8"www.\u017C\u00F3\u0142\u0107.pl");
+    CHECK_FALSE(ec);
   }
 
   SECTION("should not pct-decode the sub-delims")
@@ -29,11 +31,13 @@ TEST_CASE("Our pct-decoding function...")
     auto const input = boost::string_view("!$'()*+,;=");
 
     auto bytes = std::array<char, 256>{0};
+    auto ec    = boost::system::error_code();
 
-    auto const decoded_view =
-      boost::string_view(bytes.data(), foxy::uri::pct_decode(input, bytes.begin()) - bytes.begin());
+    auto const decoded_view = boost::string_view(
+      bytes.data(), foxy::uri::pct_decode(input, bytes.begin(), ec) - bytes.begin());
 
     CHECK(decoded_view == u8"!$'()*+,;=");
+    CHECK_FALSE(ec);
   }
 
   SECTION("should pct-decode whitespace")
@@ -41,11 +45,13 @@ TEST_CASE("Our pct-decoding function...")
     auto const input = boost::string_view("hello%20world!%0a");
 
     auto bytes = std::array<char, 256>{0};
+    auto ec    = boost::system::error_code();
 
-    auto const decoded_view =
-      boost::string_view(bytes.data(), foxy::uri::pct_decode(input, bytes.begin()) - bytes.begin());
+    auto const decoded_view = boost::string_view(
+      bytes.data(), foxy::uri::pct_decode(input, bytes.begin(), ec) - bytes.begin());
 
     CHECK(decoded_view == u8"hello world!\n");
+    CHECK_FALSE(ec);
   }
 
   SECTION("should pct-decode misc. ascii chars")
@@ -53,11 +59,13 @@ TEST_CASE("Our pct-decoding function...")
     auto const input = boost::string_view("%22%23%2f%3c%3e%3f%40%5b%5c%5d%5e%60%7b%7c%7d");
 
     auto bytes = std::array<char, 256>{0};
+    auto ec    = boost::system::error_code();
 
-    auto const decoded_view =
-      boost::string_view(bytes.data(), foxy::uri::pct_decode(input, bytes.begin()) - bytes.begin());
+    auto const decoded_view = boost::string_view(
+      bytes.data(), foxy::uri::pct_decode(input, bytes.begin(), ec) - bytes.begin());
 
     CHECK(decoded_view == u8"\"#/<>?@[\\]^`{|}");
+    CHECK_FALSE(ec);
   }
 
   SECTION("should not pct-decode the ALPHA | DIGIT | -._~ | sub-delims")
@@ -66,12 +74,14 @@ TEST_CASE("Our pct-decoding function...")
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~!$&'()*+,;=");
 
     auto bytes = std::array<char, 256>{0};
+    auto ec    = boost::system::error_code();
 
-    auto const decoded_view =
-      boost::string_view(bytes.data(), foxy::uri::pct_decode(input, bytes.begin()) - bytes.begin());
+    auto const decoded_view = boost::string_view(
+      bytes.data(), foxy::uri::pct_decode(input, bytes.begin(), ec) - bytes.begin());
 
     CHECK(decoded_view ==
           u8"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~!$&'()*+,;=");
+    CHECK_FALSE(ec);
   }
 
   SECTION("should not pct-decode select chars")
@@ -79,11 +89,13 @@ TEST_CASE("Our pct-decoding function...")
     auto const input = boost::string_view("/%22%23%3c%3e%3f@%5b%5c%5d%5e%60%7b%7c%7d:::");
 
     auto bytes = std::array<char, 256>{0};
+    auto ec    = boost::system::error_code();
 
-    auto const decoded_view =
-      boost::string_view(bytes.data(), foxy::uri::pct_decode(input, bytes.begin()) - bytes.begin());
+    auto const decoded_view = boost::string_view(
+      bytes.data(), foxy::uri::pct_decode(input, bytes.begin(), ec) - bytes.begin());
 
     CHECK(decoded_view == u8"/\"#<>?@[\\]^`{|}:::");
+    CHECK_FALSE(ec);
   }
 
   SECTION("should recognize malformed input and simply stop parsing")
@@ -100,10 +112,18 @@ TEST_CASE("Our pct-decoding function...")
 
     auto bytes = std::array<char, 256>{0};
 
-    auto const decoded_view = boost::string_view(
+    auto decoded_view = boost::string_view(
       bytes.data(), foxy::uri::pct_decode(invalid_host, bytes.begin(), ec) - bytes.begin());
 
     CHECK(decoded_view == "www.goo");
     CHECK(ec == foxy::error::unexpected_pct);
+
+    bytes.fill(0);
+
+    auto out = bytes.begin();
+    CHECK_THROWS(foxy::uri::pct_decode(invalid_host, out));
+
+    decoded_view = boost::string_view(bytes.data(), out - bytes.begin());
+    CHECK(decoded_view == "www.goo");
   }
 }
