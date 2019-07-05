e3fc0867529a8fe5e9b845ccbfdab4c428145355
LeonineKing1199 <christian.mazakas@gmail.com>
2019-07-05 09:43:50 -0700

    Removed unneeded function

--- include/foxy/utility.hpp
+++ include/foxy/utility.hpp
@@ -1,9 +1,8 @@
 //
-// Copyright (c) 2018-2019 Christian Mazakas (christian dot mazakas at gmail dot
-// com)
+// Copyright (c) 2018-2019 Christian Mazakas (christian dot mazakas at gmail dot com)
 //
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 // Official repository: https://github.com/LeonineKing1199/foxy
 //
@@ -12,13 +11,11 @@
 #define FOXY_UTILITY_HPP_
 
 #include <boost/utility/string_view.hpp>
-#include <utility>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/address.hpp>
 
 namespace foxy
 {
-auto
-parse_authority_form(boost::string_view const uri)
-  -> std::pair<std::string, std::string>;
 } // namespace foxy
 
 #endif // FOXY_UTILITY_HPP_
--- test/utility_test.cpp
+++ test/utility_test.cpp
@@ -1,9 +1,8 @@
 //
-// Copyright (c) 2018-2019 Christian Mazakas (christian dot mazakas at gmail dot
-// com)
+// Copyright (c) 2018-2019 Christian Mazakas (christian dot mazakas at gmail dot com)
 //
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 // Official repository: https://github.com/LeonineKing1199/foxy
 //
@@ -12,14 +11,4 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("Our utility library")
-{
-  SECTION("should parse the authority form of a uri for the host and port")
-  {
-    auto const uri           = "www.google.com:80";
-    auto const host_and_port = foxy::parse_authority_form(uri);
-
-    REQUIRE(std::get<0>(host_and_port) == "www.google.com");
-    REQUIRE(std::get<1>(host_and_port) == "80");
-  }
-}
+TEST_CASE("utility_test") {}
