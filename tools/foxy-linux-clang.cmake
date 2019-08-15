07952719df718275a73afb205e07bbd0ce768ad8
LeonineKing1199 <christian.mazakas@gmail.com>
2019-08-15 16:54:19 -0700

    Updated toolchain files

--- tools/foxy-linux-clang.cmake
+++ tools/foxy-linux-clang.cmake
@@ -2,6 +2,7 @@ set(CMAKE_CXX_COMPILER /usr/bin/clang++-8)
 
 set(BUILD_TESTING ON)
 set(FOXY_BUILD_EXAMPLES ON)
+set(FOXY_FUZZ ON)
 
 add_compile_definitions("BOOST_ASIO_NO_DEPRECATED")
 
@@ -12,7 +13,7 @@ add_link_options("-fsanitize=address,undefined")
 # add_link_options("-fsanitize=thread")
 
 set(Boost_USE_STATIC_LIBS ON)
-set(Boost_DIR "/home/chris/boost-spirit-develop/lib/cmake/Boost-1.71.0")
+set(Boost_DIR "/home/chris/boost-171/install/lib/cmake/Boost-1.71.0")
 
 set(CMAKE_BUILD_TYPE Debug)
 set(BUILD_SHARED_LIBS OFF)
