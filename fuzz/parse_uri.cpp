763be09f0eef34c38d59ecf823c15947e00842d2
LeonineKing1199 <christian.mazakas@gmail.com>
2019-08-14 19:00:42 -0700

    Updated URI fuzzing to include Unicode

--- fuzz/parse_uri.cpp
+++ fuzz/parse_uri.cpp
@@ -1,10 +1,19 @@
 #include <foxy/uri_parts.hpp>
 
-extern "C"
-int
-LLVMFuzzerTestOneInput(char const* data, size_t size)
+#include <vector>
+#include <cstring>
+
+extern "C" int
+LLVMFuzzerTestOneInput(char const* data, size_t const size)
 {
   auto const input = boost::string_view(data, size);
   foxy::parse_uri(input);
-  return 0;  // Non-zero return values are reserved for future use.
+
+  auto code_points = std::vector<char32_t>(size);
+  std::memcpy(code_points.data(), input.data(), size);
+
+  auto const unicode_input = boost::u32string_view(code_points.data(), code_points.size());
+  foxy::parse_uri(input);
+
+  return 0; // Non-zero return values are reserved for future use.
 }
