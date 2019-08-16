f6e9c04336f4baf690af02b779248d7bbdb47830
LeonineKing1199 <christian.mazakas@gmail.com>
2019-08-15 20:19:45 -0700

    Updated fuzz test to include Unicode

--- fuzz/parse_uri.cpp
+++ fuzz/parse_uri.cpp
@@ -9,11 +9,13 @@ LLVMFuzzerTestOneInput(char const* data, size_t const size)
   auto const input = boost::string_view(data, size);
   foxy::parse_uri(input);
 
-  auto code_points = std::vector<char32_t>(size);
-  std::memcpy(code_points.data(), input.data(), size);
+  auto const num_code_points = input.size() / 4;
+  auto       code_points     = std::vector<char32_t>(num_code_points);
+
+  std::memcpy(code_points.data(), input.data(), num_code_points * 4);
 
   auto const unicode_input = boost::u32string_view(code_points.data(), code_points.size());
-  foxy::parse_uri(input);
+  foxy::parse_uri(unicode_input);
 
   return 0; // Non-zero return values are reserved for future use.
 }
