700cc483a4c1a92e515ccf3824f3bb71f8e68428
LeonineKing1199 <christian.mazakas@gmail.com>
2019-08-27 16:49:49 -0700

    Updated comments on unicode filtering

--- fuzz/parse_uri.cpp
+++ fuzz/parse_uri.cpp
@@ -16,6 +16,9 @@ LLVMFuzzerTestOneInput(char const* data, size_t const size)
   auto code_points = std::vector<char32_t>(num_code_points);
   std::memcpy(code_points.data(), input.data(), num_code_points * sizeof(char32_t));
 
+  // manually filtering out these invalid code points will become obsolete with Boost 1.72
+  // which fixes a bug in X3's Unicode parsing functions
+  //
   auto const valid_size =
     std::remove_if(code_points.begin(), code_points.end(),
                    [](auto const code_point) -> bool { return code_point > 0x10ffff; }) -
